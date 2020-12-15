9a2473da424539c4eb5322fa652a1265fd98d502
Kalipye <70866038+Kalipye@users.noreply.github.com>
2020-12-15 18:57:49 +0100

    Add files via upload

---
+++ genres/fantasy.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+
+    <title>Genre : Fantasy</title>
+    <link rel="shortcut icon" href="../Logo2.0.png" type="image/png">
+    <link href="stylegenre.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="stylegenre.css">
+    <!--[if lt IE 9]>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <header>
+       <a href="../index.html"><img class="logo" src="../Logo2.0.png" alt="Image" /></a>
+        <h1>図書館<br>Toshokan</h1>
+        <nav>
+          <ul>
+            <li><a href="../index.php"><strong>ACCUEIL</strong></a></li>
+            <li class="deroulant"><a href="#"><strong>GENRES</strong></a>
+              <ul class="sous">
+                <li><a href="action.php"><strong>Action</strong></a></li>
+                <li><a href="aventure.php"><strong>Aventure</strong></a></li>
+                <li><a href="comedie.php"><strong>Comédie</strong></a></li>
+                <li><a href="drama.php"><strong>Drama</strong></a></li>
+                <li><a href="fantastique.php"><strong>Fantastique</strong></a></li>
+                <li><a href="fantasy.php"><strong>Fantasy</strong></a></li>
+                <li><a href="horreur.php"><strong>Horreur</strong></a></li>
+                <li><a href="mature.php"><strong>Mature</strong></a></li>
+                <li><a href="psychologique.php"><strong>Psychologique</strong></a></li>
+                <li><a href="romance.php"><strong>Romance</strong></a></li>
+                <li><a href="school_life.php"><strong>School life</strong></a></li>
+                <li><a href="sf.php"><strong>S.F</strong></a></li>
+                <li><a href="slice_life.php"><strong>Slice of life</strong></a></li>
+                <li><a href="surnaturel.php"><strong>Surnaturel</strong></a></li>
+              </ul> 
+            </li>
+            <li><a href="../equipe/equipe.php"><strong>EQUIPE</strong></a></li>
+            <li><a href="#"><strong>CONTACT</strong></a></li>
+          </ul>
+     </nav>
+    </header>
+        <main>
+      
+         <article class="bodyy">
+      <div class="container">
+        <div class="genre">
+        <h2>Fantasy :</h2> <p>C’est un genre présentant un ou plusieurs éléments surnaturels qui relèvent souvent du mythe et qui sont souvent incarnés par l'irruption ou l'utilisation de la magie. (Wikipédia)</p>
+        </div>
+
+        <div class="boxg">
+          <div class="imgBox">
+            <img src="../img/TatenoYuusha.jpg" alt="TatenoYuusha image">
+          </div>
+          <div class="details">
+            <div class="content">
+            <h3>The Rising Shield Hero</h3>
+            <ul>
+<li>Éditeur VF : Doki-Doki (Doki-Doki - Seinen)</li>
+<li>Éditeur VO : Media Factory</li>
+<li>Illustrations : N&B</li>
+<li>Code ISBN: 9782818936238</li>
+<li>Scénariste : Aneko Yusagi</li>
+<li>Dessinateur : Aiya Kyu</li>
+<li>Chara-designer : Minami Seira</li>
+<li>Type : Seinen</li>
+<li>Genres : Action - Aventure - Drame – Fantasy</li>
+            </ul>
+            </div>
+          </div> 
+        </div>
+      </div>
+    </article>
+
+      <aside>
+        <h2>Notre Sélection</h2>
+        <!-- pas de puce stp -->
+        <ul style="list-style-type: none;">
+  <li><a href="../pages/accel.php">Accel World</a></li>
+          <li><a href="../pages/bakuman.php">Bakuman</a></li>
+          <li><a href="../pages/beastars.php">Beastars</a></li>
+          <li><a href="../pages/bleach.php">Bleach</a></li>
+          <li><a href="../pages/dgraym.php">D.Gray-Man</a></li>
+          <li><a href="../pages/darling.php">Darling in the FRANXX</a></li>
+          <li><a href="../pages/9782752300553.php">Excel Saga</a></li>
+          <li><a href="../pages/9782820316585.php">Haikyuu</a></li>
+          <li><a href="../pages/9782375060247.php">Overlord</a></li>
+          <li><a href="../pages/9782505066903.php">Sky High Survival</a></li>
+          <li><a href="../pages/9782351420553.php">Soul Eater</a></li>
+          <li><a href="../pages/9782818936238.php">Tate no Yuusha no Nariagi</a></li>
+        </ul>
+      </aside>
+    </main>
+  </body>
+</html>
