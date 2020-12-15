243235f54f0eb56e689b7285283afead56669be3
Kalipye <70866038+Kalipye@users.noreply.github.com>
2020-12-15 19:00:03 +0100

    equipe

---
+++ equipe/equipe.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+
+    <title>図書館 - Toshokan</title>
+    <link rel="shortcut icon" href="../Logo2.0.png" type="image/png">
+    <link href="stylequipe.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="stylequipe.css">
+    <!--[if lt IE 9]>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <header>
+        <img class="logo" src="../Logo2.0.png" alt="Image" />
+        <h1>図書館<br>Toshokan</h1>
+        <nav>
+          <ul>
+            <li><a href="../index.php"><strong>ACCUEIL</strong></a></li>
+            <li class="deroulant"><a href="#"><strong>GENRES</strong></a>
+             <ul class="sous">
+                <li><a href="../genres/action.php"><strong>Action</strong></a></li>
+                <li><a href="../genres/aventure.php"><strong>Aventure</strong></a></li>
+                <li><a href="../genres/comedie.php"><strong>Comédie</strong></a></li>
+                <li><a href="../genres/drama.php"><strong>Drama</strong></a></li>
+                <li><a href="../genres/fantastique.php"><strong>Fantastique</strong></a></li>
+                <li><a href="../genres/fantasy.php"><strong>Fantasy</strong></a></li>
+                <li><a href="../genres/horreur.php"><strong>Horreur</strong></a></li>
+                <li><a href="../genres/mature.php"><strong>Mature</strong></a></li>
+                <li><a href="../genres/psychologique.php"><strong>Psychologique</strong></a></li>
+                <li><a href="../genres/romance.php"><strong>Romance</strong></a></li>
+                <li><a href="../genres/school_life.php"><strong>School life</strong></a></li>
+                <li><a href="../genres/sf.php"><strong>S.F</strong></a></li>
+                <li><a href="../genres/slice_life.php"><strong>Slice of life</strong></a></li>
+                <li><a href="../genres/surnaturel.php"><strong>Surnaturel</strong></a></li>
+              </ul> 
+            </li>
+            <li><a href="equipe.php"><strong>EQUIPE</strong></a></li>
+            <li><a href="#"><strong>CONTACT</strong></a></li>
+          </ul>
+     </nav>
+    </header>
+       
+    <main class="container">
+      <h2>Équipe</h2>
+      <h3>Bienvenue sur notre site, <br> nous sommes : <br> Adrien Delannoy et Alina Phothivong.</h3>
+      <p>Nous sommes étudiants de l'école IPSSI en première année de BTS SIO <br>et ce site est créer dans le cadre d'un projet d'un cours à l'IPSSI.</p>
+      </main>
+    </body>
+    </html>
