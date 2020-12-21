76b5bd51966c8fa1e945a34a245a691efc79815f
Kalipye <70866038+Kalipye@users.noreply.github.com>
2020-12-21 22:34:49 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,27 +1,18 @@
-        <img class="logo" src="/Toshokan/img/Logo2.0.png" alt="Image" />
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+
+    <title>図書館 - Toshokan</title>
+    <link rel="shortcut icon" href="/biblio_groupe15_etape3/img/Logo2.0.png" type="image/png">
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="style.css">
+    <!--[if lt IE 9]>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <header>
+        <img class="logo" src="/biblio_groupe15_etape3/img/Logo2.0.png" alt="Image" />
         <h1>図書館<br>Toshokan</h1>
-        <nav>
-          <ul>
-            <li><a href="#"><strong>ACCUEIL</strong></a></li>
-            <li class="deroulant"><a href="#"><strong>GENRES</strong></a>
-              <ul class="sous">
-                <li><a href="#"><strong>Action</strong></a></li>
-                <li><a href="#"><strong>Aventure</strong></a></li>
-                <li><a href="#"><strong>Comédie</strong></a></li>
-                <li><a href="#"><strong>Drama</strong></a></li>
-                <li><a href="#"><strong>Fantastique</strong></a></li>
-                <li><a href="#"><strong>Fantasy</strong></a></li>
-                <li><a href="#"><strong>Horreur</strong></a></li>
-                <li><a href="#"><strong>Mature</strong></a></li>
-                <li><a href="#"><strong>Psychologique</strong></a></li>
-                <li><a href="#"><strong>Romance</strong></a></li>
-                <li><a href="#"><strong>School life</strong></a></li>
-                <li><a href="#"><strong>S.F</strong></a></li>
-                <li><a href="#"><strong>Slice of life</strong></a></li>
-                <li><a href="#"><strong>Surnaturel</strong></a></li>
-              </ul> 
-            </li>
-            <li><a href="#"><strong>EQUIPE</strong></a></li>
-            <li><a href="#"><strong>CONTACT</strong></a></li>
-          </ul>
-        </nav>
