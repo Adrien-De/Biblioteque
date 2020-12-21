a5fde897f4110588ca5f5a9c9b866114301bd00b
Kalipye <70866038+Kalipye@users.noreply.github.com>
2020-12-21 22:12:31 +0100

    Update mature.php

--- genres/mature.php
+++ genres/mature.php
@@ -98,19 +98,19 @@
       <aside>
         <h2>Notre Sélection</h2>
         <!-- pas de puce stp -->
-        <ul style="list-style-type: none;">
-  <li><a href="../pages/accel.php">Accel World</a></li>
-          <li><a href="../pages/bakuman.php">Bakuman</a></li>
-          <li><a href="../pages/beastars.php">Beastars</a></li>
-          <li><a href="../pages/bleach.php">Bleach</a></li>
-          <li><a href="../pages/dgraym.php">D.Gray-Man</a></li>
-          <li><a href="../pages/darling.php">Darling in the FRANXX</a></li>
-          <li><a href="../pages/9782752300553.php">Excel Saga</a></li>
-          <li><a href="../pages/9782820316585.php">Haikyuu</a></li>
-          <li><a href="../pages/9782375060247.php">Overlord</a></li>
-          <li><a href="../pages/9782505066903.php">Sky High Survival</a></li>
-          <li><a href="../pages/9782351420553.php">Soul Eater</a></li>
-          <li><a href="../pages/9782818936238.php">Tate no Yuusha no Nariagi</a></li>
+                <ul style="list-style-type: none;">
+          <li><a href="/biblio_groupe15_etape3/pages/9780316376730.php">Accel World</a></li>
+          <li><a href="/biblio_groupe15_etape3/pages/9782505008262.php">Bakuman</a></li>
+          <li><a href="/biblio_groupe15_etape3/pages/9791032703793.php">Beastars</a></li>
+          <li><a href="/biblio_groupe15_etape3/pages/9782723442275.php">Bleach</a></li>
+          <li><a href="/biblio_groupe15_etape3/pages/9782723455848.php">D.Gray-Man</a></li>
+          <li><a href="/biblio_groupe15_etape3/pages/darling.php">Darling in the FRANXX</a></li>
+          <li><a href="/biblio_groupe15_etape3/pages/9782752300553.php">Excel Saga</a></li>
+          <li><a href="/biblio_groupe15_etape3/pages/9782820316585.php">Haikyuu</a></li>
+          <li><a href="/biblio_groupe15_etape3/pages/9782375060247.php">Overlord</a></li>
+          <li><a href="/biblio_groupe15_etape3/pages/9782505066903.php">Sky High Survival</a></li>
+          <li><a href="/biblio_groupe15_etape3/pages/9782351420553.php">Soul Eater</a></li>
+          <li><a href="/biblio_groupe15_etape3/pages/9782818936238.php">Tate no Yuusha no Nariagi</a></li>
         </ul>
       </aside>
     </main>
