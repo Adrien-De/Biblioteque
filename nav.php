2135e1b2937faf8a73f50a7f60aedc68f843ae3d
Kalipye <70866038+Kalipye@users.noreply.github.com>
2020-12-21 22:36:05 +0100

    nav

---
+++ nav.php
@@ -0,0 +1,27 @@
+
+        <nav>
+          <ul>
+            <li><a href="/biblio_groupe15_etape3/index.php"><strong>ACCUEIL</strong></a></li>
+            <li class="deroulant"><a href="#"><strong>GENRES</strong></a>
+                  <ul class="sous">
+                <li><a href="/biblio_groupe15_etape3/genres/action.php"><strong>Action</strong></a></li>
+                <li><a href="/biblio_groupe15_etape3/genres/aventure.php"><strong>Aventure</strong></a></li>
+                <li><a href="/biblio_groupe15_etape3/genres/comedie.php"><strong>Comédie</strong></a></li>
+                <li><a href="/biblio_groupe15_etape3/genres/drama.php"><strong>Drama</strong></a></li>
+                <li><a href="/biblio_groupe15_etape3/genres/fantastique.php"><strong>Fantastique</strong></a></li>
+                <li><a href="/biblio_groupe15_etape3/genres/fantasy.php"><strong>Fantasy</strong></a></li>
+                <li><a href="/biblio_groupe15_etape3/genres/horreur.php"><strong>Horreur</strong></a></li>
+                <li><a href="/biblio_groupe15_etape3/genres/mature.php"><strong>Mature</strong></a></li>
+                <li><a href="/biblio_groupe15_etape3/genres/psychologique.php"><strong>Psychologique</strong></a></li>
+                <li><a href="/biblio_groupe15_etape3/genres/romance.php"><strong>Romance</strong></a></li>
+                <li><a href="/biblio_groupe15_etape3/genres/school_life.php"><strong>School life</strong></a></li>
+                <li><a href="/biblio_groupe15_etape3/genres/sf.php"><strong>S.F</strong></a></li>
+                <li><a href="/biblio_groupe15_etape3/genres/slice_life.php"><strong>Slice of life</strong></a></li>
+                <li><a href="/biblio_groupe15_etape3/genres/surnaturel.php"><strong>Surnaturel</strong></a></li>
+              </ul> 
+            </li>
+            <li><a href="/biblio_groupe15_etape3/equipe/equipe.php"><strong>EQUIPE</strong></a></li>
+            <li><a href="/biblio_groupe15_etape3/contact/contact.php"><strong>CONTACT</strong></a></li>
+          </ul>
+        </nav>
+    </header>
